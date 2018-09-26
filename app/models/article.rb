f5aa10938d13e761d25c0c441b78df24fa0349ae
romainbodelot <romain.bodelot@viacesi.fr>
2018-09-26 09:50:17 +0200

    100% finish

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ApplicationRecord
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
+  has_one_attached :image
   def tag_list
     self.tags.collect do |tag|
       tag.name
